41284b0007e4c927d0538c658f4f55ce41acf604
j-yuvraj <100347652+j-yuvraj@users.noreply.github.com>
2024-12-23 18:44:37 -0600

    Update streamlit_app.py
    Added fruit chosen info

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,12 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen+ ' '
         
-    #st.write(ingredients_string)
+        # new section to display smoothiefroot nutrition information
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        # st.text(smoothiefroot_response.json())
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width= True)
+
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order+ """')"""
@@ -53,9 +58,5 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
         
-# new section to display smoothiefroot nutrition information
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width= True)
 
 
